a3b6cb8fd5295ba6fc06f604e44649d1ac34d9ab
Jinho <zinoah@gmail.com>
2022-12-29 15:18:41 +0900

    second commit

---
+++ src/edu/kh/collection/run/MepRun.java
@@ -0,0 +1,16 @@
+package edu.kh.collection.run;
+
+import edu.kh.collection.model.service.MapService;
+
+public class MepRun {
+
+	public static void main(String[] args) {
+		
+		MapService map = new MapService();
+		
+		//map.ex1();
+		//map.ex2();
+		map.ex3();
+	}
+
+}
---
+++ src/edu/kh/collection/run/SetRun.java
@@ -0,0 +1,18 @@
+package edu.kh.collection.run;
+
+import edu.kh.collection.model.service.Setservice;
+
+public class SetRun {
+	public static void main(String[] args) {
+		
+		Setservice set = new Setservice();
+		
+		//set.ex1();
+		//set.ex2();
+		//set.ex3();
+		//set.ex4();
+		//set.ex5();
+		set.lotto();
+		
+	}
+}
